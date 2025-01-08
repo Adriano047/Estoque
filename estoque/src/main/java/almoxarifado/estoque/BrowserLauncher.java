a16e36429902f43e6c14be49720f961882fa61e2
Adriano047 <106997880+Adriano047@users.noreply.github.com>
2025-01-08 08:18:53 -0300

    Update BrowserLauncher.java

--- estoque/src/main/java/almoxarifado/estoque/BrowserLauncher.java
+++ estoque/src/main/java/almoxarifado/estoque/BrowserLauncher.java
@@ -1,15 +1,19 @@
 package almoxarifado.estoque;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BrowserLauncher implements CommandLineRunner {
+    @Value("${server.port:8080}")
+    private int port;
 
     @Override
     public void run(String... args) {
-        String url = "http://localhost:8080";
+        String url = "http://localhost:" + port;
         System.out.println("O servidor está rodando!");
         System.out.println("Abra o navegador e acesse: " + url);
     }
 }
+
